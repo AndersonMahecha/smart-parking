85a8d1e73c964e1eeebe29861c339ed767f0a3bd
Anderson Mahecha <andhymg97@gmail.com>
2024-11-19 11:18:48 -0500

    Added slots driver.

---
+++ devices/esp32/parking_slots_driver/src/main.cpp
@@ -0,0 +1,165 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include <WebSocketsClient.h>
+
+// definicion de tipos de mensaje
+#define MESSAGE_TYPE_STATUS 0
+#define MESSAGE_TYPE_COMMAND 1
+#define MESSAGE_TYPE_RESPONSE 2
+#define MESSAGE_TYPE_ERROR 3
+#define MESSAGE_TYPE_ENTRY 4
+#define MESSAGE_TYPE_EXIT 5
+
+// definicion de tipos de dispositivo
+#define DEVICE_TYPE_BARRIER 0
+#define DEVICE_TYPE_SENSOR 1
+
+#define SENSE_UMBRAL 2000
+#define SENSE_COUNT 4
+
+#define ssid "FamiliaMahechaSalamanca"
+#define password "Nirvana77"
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length);
+
+uint8_t SENSOR_PINS[SENSE_COUNT] = {
+    36,
+    39,
+    34,
+    35
+    };
+
+bool SENSOR_STATES[SENSE_COUNT] = {false};
+unsigned long lastChangeTime[SENSE_COUNT] = {0};
+const unsigned long debounceTime = 1000; // Tiempo en milisegundos
+
+WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
+void buildMessage();
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Serial.println("Starting WiFi");
+  Serial.print("[WiFi] Connecting to ");
+  Serial.println(ssid);
+
+  WiFiMulti.addAP(ssid, password);
+
+  pinMode(36, INPUT);
+  pinMode(39, INPUT);
+  pinMode(34, INPUT);
+  pinMode(35, INPUT);
+
+  while (WiFiMulti.run() != WL_CONNECTED)
+  {
+    delay(100);
+  }
+
+  String ip = WiFi.localIP().toString();
+  Serial.printf("[SETUP] WiFi Connected %s\n", ip.c_str());
+
+  webSocket.begin("192.168.10.27", 3500, "");
+
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+
+  // try ever 5000 again if connection has failed
+  webSocket.setReconnectInterval(1000);
+}
+
+void loop()
+{
+  webSocket.loop();
+
+  bool changeDetected = false;
+
+  for (int i = 0; i < SENSE_COUNT; i++)
+  {
+
+    bool currentState = analogRead(SENSOR_PINS[i]) > SENSE_UMBRAL;
+
+    if (currentState != SENSOR_STATES[i] && millis() - lastChangeTime[i] > debounceTime)
+    {
+      lastChangeTime[i] = millis();
+      SENSOR_STATES[i] = currentState;
+      changeDetected = true;
+    }
+  }
+
+  if (changeDetected)
+  {
+    buildMessage();
+  }
+}
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[WSc] Disconnected!\n");
+    break;
+  case WStype_CONNECTED:
+    Serial.printf("[WSc] Connected to url: %s\n", payload);
+
+    // send message to server when Connected
+    webSocket.sendTXT("Connected");
+    break;
+  case WStype_TEXT:
+
+    break;
+  case WStype_ERROR:
+    break;
+  case WStype_FRAGMENT_TEXT_START:
+    break;
+  case WStype_FRAGMENT_BIN_START:
+    break;
+  case WStype_FRAGMENT:
+    break;
+  case WStype_FRAGMENT_FIN:
+    break;
+  }
+}
+
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16)
+{
+  const uint8_t *src = (const uint8_t *)mem;
+  Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+  for (uint32_t i = 0; i < len; i++)
+  {
+    if (i % cols == 0)
+    {
+      Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+    }
+    Serial.printf("%02X ", *src);
+    src++;
+  }
+  Serial.printf("\n");
+}
+
+// Definicion de protocolo de comunicacion
+// 3 bytes de longitud
+// 1 byte para el tipo de mensaje
+// 1 byte para el tipo de dispositivo
+// 1 byte para el estado de los sensores
+
+void buildMessage()
+{
+  u8_t message[7];
+  message[0] = MESSAGE_TYPE_STATUS;
+  message[1] = DEVICE_TYPE_SENSOR;
+  // write the sensor states to the message
+  for (int i = 0; i < SENSE_COUNT; i++)
+  {
+    message[i + 2] = SENSOR_STATES[i];
+  }
+  hexdump(message, 7);
+  webSocket.sendBIN(message, 7);
+}
--- socket-server/main.py
+++ socket-server/main.py
@@ -22,16 +22,21 @@ class Parking(object):
         self.current_vehicles_count += 1
         return True
 
+
 def random_id():
     return "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
+
 class Client(object):
     def __init__(self, client_id, websocket):
+        self.client_type = "device"
         self.client_id = client_id
         self.websocket = websocket
         self.events = asyncio.Queue()
 
+
 clients: List[Client] = []
+
 
 async def notify_client(client):
     response = build_barrier_status_response(False)
@@ -47,24 +52,50 @@ async def notify_client(client):
             return
 
 
-def notify_clients():
-    pass
-
 _MESSAGE_TYPE_STATUS = 0
 _MESSAGE_TYPE_RESPONSE = 2
 _MESSAGE_TYPE_ENTRY = 4
 _MESSAGE_TYPE_EXIT = 5
 
-def process_message(message: bytes) -> str:
+import json
+
+
+def build_browser_message():
+
+    return json.dumps(
+        {
+            "max_vehicles": parking.max_vehicles,
+            "current_vehicles_count": parking.current_vehicles_count,
+            "is_occupied_slots": parking.is_occupied_slots,
+            "cards_id": parking.cards_id,
+        }
+    )
+
+
+def notify_browsers():
+    for client in clients:
+        if client.client_type == "browser":
+            message = build_browser_message()
+            client.events.put_nowait(message)
+
+
+def process_message(message: bytes) -> str | None:
     message_type = message[0]
     device_type = message[1]
-    status = message[2]
-    card_id = message[3:13].decode('utf-8')
 
     if message_type == _MESSAGE_TYPE_ENTRY:
+        card_id = message[3:13].decode("utf-8")
+        status = message[2]
         ok = parking.add_vehicle(card_id)
         response = build_barrier_status_response(ok)
+        notify_browsers()
         return response
+    if message_type == _MESSAGE_TYPE_STATUS:
+        for idx, status in enumerate(parking.is_occupied_slots):
+            occupied = status == 0
+            parking.is_occupied_slots[idx] = occupied
+        notify_browsers()
+        return None
 
 
 def build_barrier_status_response(ok):
@@ -77,7 +108,7 @@ def build_barrier_status_response(ok):
     for card_id in parking.cards_id:
         for c in card_id:
             response.append(ord(c))
-    return response.decode('utf-8')
+    return response.decode("utf-8")
 
 
 async def handler(websocket):
@@ -97,12 +128,18 @@ async def handler(websocket):
                         c.events.put_nowait(message)
                 client.events.put_nowait("notified")
                 continue
+            if message == "browser":
+                client.client_type = "browser"
+                print(f"Client {client_id} is a browser")
+                client.events.put_nowait(build_browser_message())
+                continue
             if message == "Connected":
                 print(message)
                 continue
 
             response = process_message(message)
-            client.events.put_nowait(response)
+            if response is not None:
+                client.events.put_nowait(response)
 
             print(f"Client {client_id} received message: {message}")
             print(f"Client {client_id} response: {response}")
@@ -119,7 +156,7 @@ async def websocket_handler():
     async with websockets.serve(handler, port=3500):
         await asyncio.Future()
 
+
 parking = Parking()
 if __name__ == "__main__":
     asyncio.run(websocket_handler())
-
